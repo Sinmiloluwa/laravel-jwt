641fac6cf0bd69f4df483dc48708f3ccbfe593df
Sinmiloluwa <mofeoluwae@gmail.com>
2025-01-17 15:40:07 +0100

    clear config

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,12 @@ echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
 php artisan config:cache
+
 
 echo "Caching routes..."
 php artisan route:cache
